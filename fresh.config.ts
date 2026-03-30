acf68a36a64fadc9b7395a86fed1f1644b90f2dc
Myka White <mwhite454@gmail.com>
2026-03-30 11:15:05 -0400

    chore: remove denostories plugin from fresh.config.ts

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,8 +1,3 @@
 import type { FreshConfig } from "$fresh/server.ts";
-import denostories from "https://deno.land/x/denostories@0.3.0/mod.ts";
 
-export default {
-  plugins: [
-    denostories(),
-  ],
-} as FreshConfig;
+export default {} satisfies FreshConfig;
